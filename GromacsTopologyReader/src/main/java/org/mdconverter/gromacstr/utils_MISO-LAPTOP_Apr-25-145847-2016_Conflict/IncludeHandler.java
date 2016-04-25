962d6012a0a3e01ca35ea62038e40c824e20d0a7
Michael <se14m014@technikum-wien.at>
2016-04-25 15:09:49 +0200

    package naming corretions

---
+++ GromacsTopologyReader/src/main/java/org/mdconverter/gromacstr/utils_MISO-LAPTOP_Apr-25-145847-2016_Conflict/IncludeHandler.java
@@ -0,0 +1,52 @@
+package org.mdconverter.gromacstr.utils;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+/**
+ * Created by miso on 07.12.2015.
+ */
+public class IncludeHandler {
+
+    /**
+     * Loads ForceField files from Gromacs
+     *
+     * @param clazz class method getting called from
+     * @param name  the name of the file
+     * @return a byte Array containing the requested file
+     * @throws URISyntaxException | IOException
+     */
+    public static byte[] getFileByName(Class clazz, String name) throws URISyntaxException, IOException {
+        //name = name.replace("\\", File.separator);
+        String path = "top/" + name;
+        Path filePath = Paths.get(clazz.getProtectionDomain().getCodeSource().getLocation().toURI());
+        if (Files.isExecutable(filePath)) {  // Run with JAR file
+            final JarFile jar = new JarFile(filePath.toFile());
+            final Enumeration<JarEntry> entries = jar.entries(); //gives ALL entries in jar
+            while (entries.hasMoreElements()) {
+                JarEntry jarEntry = entries.nextElement();
+                String entryName = jarEntry.getName();
+                if (entryName.endsWith(path)) { //filter according to the path
+                    InputStream stream = jar.getInputStream(jarEntry);
+                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                    byte[] buffer = IOUtils.toByteArray(stream);
+                    outputStream.write(buffer);
+                    jar.close();
+                    return outputStream.toByteArray();
+                }
+            }
+            jar.close();
+        }
+        return null;
+    }
+}
